5a26a5b006c42e5df9ef42f930d68be1a366d395
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-04-02 13:11:19 +0000

    Update audio file paths

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-console.log("Welcome to Tic Tac Toe")
-let music = new Audio("music.mp3")
-let audioTurn = new Audio("ting.mp3")
-let gameover = new Audio("gameover.mp3")
+let music = new Audio("public/music.mp3")
+let audioTurn = new Audio("public/ting.mp3")
+let gameover = new Audio("public/gameover.mp3")
 let turn = "X"
 let isgameover = false;
 
